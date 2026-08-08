3d1ce249105d2b6f74d143f5d2c9b5cbb20a9c62
Samuel Omojiade <omojiadesamuel@gmail.com>
2026-08-08 17:55:02 +0100

    docs: add grading outputs and fix terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,41 +1,24 @@
-output "vpc_id" {
-  description = "ID of the Project Bedrock VPC."
-  value       = module.vpc.vpc_id
-}
-output "eks_cluster_name" {
-  description = "EKS cluster name."
-  value       = module.eks.cluster_name
-}
-
-output "eks_cluster_endpoint" {
-  description = "EKS cluster API endpoint."
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
   value       = module.eks.cluster_endpoint
 }
 
-output "eks_cluster_version" {
-  description = "EKS Kubernetes version."
-  value       = module.eks.cluster_version
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
 }
 
-output "eks_node_group_name" {
-  description = "EKS managed node group name."
-  value       = module.eks.node_group_name
-}
-output "catalog_rds_endpoint" {
-  description = "MySQL endpoint for the Catalog service."
-  value       = aws_db_instance.catalog_mysql.address
+output "region" {
+  description = "AWS region"
+  value       = var.region
 }
 
-output "orders_rds_endpoint" {
-  description = "PostgreSQL endpoint for the Orders service."
-  value       = aws_db_instance.orders_postgres.address
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
 }
 
-output "carts_table_name" {
-  description = "DynamoDB table used by the Carts service."
-  value       = aws_dynamodb_table.carts.name
-}
 output "assets_bucket_name" {
-  description = "Name of the S3 assets bucket"
+  description = "S3 assets bucket name"
   value       = aws_s3_bucket.assets.bucket
 }
