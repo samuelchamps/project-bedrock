c86066495f45c6500a7747c7ac89542609cae847
Samuel Omojiade <omojiadesamuel@gmail.com>
2026-08-16 08:23:17 +0100

    docs: finalize project bedrock deployment and evidence

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,3 +38,54 @@ module "vpc" {
     Project = "tinyuka-2025-capstone"
   }
 }
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "project-bedrock-eks"
+  kubernetes_version = "1.33"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  endpoint_public_access  = true
+  endpoint_private_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  addons = {
+    vpc-cni = {
+      most_recent    = true
+      before_compute = true
+    }
+
+    kube-proxy = {
+      most_recent    = true
+      before_compute = true
+    }
+
+    coredns = {
+      most_recent    = true
+      before_compute = true
+    }
+  }
+
+  eks_managed_node_groups = {
+    default = {
+      name = "project-bedrock-nodes"
+
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      subnet_ids = module.vpc.private_subnets
+    }
+  }
+
+  tags = {
+    Project = "tinyuka-2025-capstone"
+  }
+}
