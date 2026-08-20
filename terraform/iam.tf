b7aa2ecb50e109bfa27773d9075d7bfaf24827e3
Samuel Omojiade <omojiadesamuel@gmail.com>
2026-08-20 12:21:33 +0100

    Add namespace-scoped EKS access for grading user

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -29,3 +29,29 @@ resource "aws_iam_user_policy" "bedrock_dev_s3_upload" {
     ]
   })
 }
+
+resource "aws_eks_access_entry" "bedrock_dev_view" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = aws_iam_user.bedrock_dev_view.arn
+  type          = "STANDARD"
+
+  tags = {
+    Project = "tinyuka-2025-capstone"
+  }
+}
+
+resource "aws_eks_access_policy_association" "bedrock_dev_view" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = aws_eks_access_entry.bedrock_dev_view.principal_arn
+
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+
+  access_scope {
+    type       = "namespace"
+    namespaces = ["retail-app"]
+  }
+
+  depends_on = [
+    aws_eks_access_entry.bedrock_dev_view
+  ]
+}
